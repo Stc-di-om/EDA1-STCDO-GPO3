e476ebfa98cf585e52479224e507952814ca4425
Stc-di-om <sanchezdetaglecaballerodiegoom@gmail.com>
2025-11-26 12:38:42 -0600

    Create Variables_cadenas.py

---
+++ Practica 9/Variables_cadenas.py
@@ -0,0 +1,15 @@
+# Ejemplos de variables y cadenas
+
+mensaje = "Hola Mundo"
+numero = 10
+pi = 3.1416
+
+print(mensaje)
+print("El número es:", numero)
+print("El valor de pi es: {}".format(pi))
+
+nombre = "Diego"
+apellido = "Sánchez"
+
+print("Mi nombre completo es {} {}".format(nombre, apellido))
+print("Orden inverso: {1} {0}".format(nombre, apellido))
