c2a0ca5184de89ee72c52ba6881c72f1df624b8b
Stc-di-om <sanchezdetaglecaballerodiegoom@gmail.com>
2025-11-26 12:42:41 -0600

    Create ciclos.py

---
+++ Practica 9/ciclos.py
@@ -0,0 +1,15 @@
+# Mientras (while)
+contador = 0
+while contador < 5:
+    print("Contador:", contador)
+    contador += 1
+
+# For en lista
+frutas = ["manzana", "pera", "uva"]
+for fruta in frutas:
+    print("Fruta:", fruta)
+
+# For en diccionario
+persona = {"nombre": "Ana", "edad": 25}
+for llave, valor in persona.items():
+    print(llave, ":", valor)
