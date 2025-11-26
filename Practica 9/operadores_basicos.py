c72037a34a3ba962879a092fd80c278db9c817e1
Stc-di-om <sanchezdetaglecaballerodiegoom@gmail.com>
2025-11-26 12:39:27 -0600

    Create operadores_basicos.py

---
+++ Practica 9/operadores_basicos.py
@@ -0,0 +1,13 @@
+# Operadores aritméticos
+a = 10
+b = 3
+
+print("Suma:", a + b)
+print("Resta:", a - b)
+print("Multiplicación:", a * b)
+print("División:", a / b)
+
+# Operadores booleanos
+print("a > b:", a > b)
+print("a == b:", a == b)
+print("a != b:", a != b)
