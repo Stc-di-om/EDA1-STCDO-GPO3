138f05f74ff0ede6d5ce0807c9bd06d40f064c20
Stc-di-om <sanchezdetaglecaballerodiegoom@gmail.com>
2025-11-26 12:40:23 -0600

    Create listas_tuplas_diccionarios.py

---
+++ Practica 9/listas_tuplas_diccionarios.py
@@ -0,0 +1,16 @@
+# Listas
+lista = [1, 2, 3, 4]
+print("Lista:", lista)
+lista.append(5)
+print("Lista modificada:", lista)
+
+# Tuplas
+tupla = (10, 20, 30)
+print("Tupla:", tupla)
+
+# Diccionarios
+dic = {"nombre": "Diego", "edad": 20}
+print("Diccionario:", dic)
+print("Nombre:", dic["nombre"])
+dic["edad"] = 21
+print("Diccionario modificado:", dic)
